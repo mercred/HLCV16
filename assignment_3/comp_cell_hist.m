c21e8c206386100528a7b368de2341515169e89b
AleksanderSas <aleksander.sas93@gmail.com>
2016-05-21 12:13:52 +0200

    assignment 3, exercise 2

---
+++ assignment_3/comp_cell_hist.m
@@ -0,0 +1,39 @@
+%
+% Compute gradient orientation histogram for one cell of the HOG descriptor.
+% Each pixel should contribute to corresponding orientation bin with magnitude
+% of its gradient. 
+%
+% parameters: 
+% 
+% img_cell_mag - matrix of gradient magnitudes for each pixel on the cell
+% img_cell_ori - matrix of gradient orientations for each pixel of the cell
+%
+% PARAMS.hist_binsize - number of bins in the gradient orientation histogram
+% PARAMS.hist_min - minimal orientation of the gradient vector
+% PARAMS.hist_max - maximal orientation of the gradient vector
+%
+%
+% note: see detector_param.m for the definition of PARAMS
+%
+
+function h = comp_cell_hist(PARAMS, img_cell_mag, img_cell_ori)
+
+  img_cell_mag = img_cell_mag(:);
+  img_cell_ori = img_cell_ori(:);
+
+  img_cell_ori(img_cell_ori < PARAMS.hist_min) = img_cell_ori(img_cell_ori < PARAMS.hist_min) + 1e-6;
+  img_cell_ori(img_cell_ori >= PARAMS.hist_max) = img_cell_ori(img_cell_ori >= PARAMS.hist_max) - 1e-6;
+
+  assert(all(img_cell_ori >= PARAMS.hist_min));
+  assert(all(img_cell_ori < PARAMS.hist_max));
+
+  h = zeros(1,PARAMS.hist_numbins);
+  
+  img_cell_ori = floor((img_cell_ori + pi) / (2*pi / PARAMS.hist_numbins)) + 1;
+  
+  for i = 1:length(img_cell_ori)
+    h(img_cell_ori(i)) = h(img_cell_ori(i)) + img_cell_mag(i);
+  end
+  
+
+  % note: do not normalize the histogram at this point yet
