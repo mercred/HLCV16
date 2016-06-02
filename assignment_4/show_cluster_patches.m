c35c0c4dcfa2524a9829ffa0bd5205a3bfdfdbf9
AleksanderSas <aleksander.sas93@gmail.com>
2016-06-02 19:30:19 +0200

    fix bug

--- assignment_4/show_cluster_patches.m
+++ assignment_4/show_cluster_patches.m
@@ -5,11 +5,11 @@ function show_cluster_patches(images, assignments, cluster_idx)
       num_clusters = 7;
       image_number_per_row = 7;
       
-      for i = 1:randi([1,100],1,num_clusters)
+      for i = 1:num_clusters
         indexes = find(assignments == i);
         indexes = indexes(1:min(length(indexes), image_number_per_row));
         
-        for k = 1:length(indexes)
+        for k = 1:image_number_per_row
             subplot(num_clusters, image_number_per_row, (i-1)*image_number_per_row+k);
             imshow(images{indexes(k)});
         end
